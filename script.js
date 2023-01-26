7869a9ef12727113b04166c3331f460f3b0f08b0
Javier Damiani <javierdamiani74@gmail.com>
2023-01-25 19:44:27 -0500

    project5_first-part
    Se realizó el popUp del botón Add, asimismo, se ha hecho que las tarjetas carguen de acuerdo a JS

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const editBtn = document.querySelector(".profile__info-edit_button");
-const popUp = document.querySelector(".popup");
-const closePopUpBtn = document.querySelector(
-  ".popup__container-form-toggle_close"
-);
+const editBtn = document.querySelector("#editButton");
+const popUp = document.querySelector("#popUpProfile");
+const closePopUpBtn = document.querySelector("#closeBtn");
 //Variables para jalar el nombre de la web
-const profileInfoName = document.querySelector(".profile__info-name");
-const profileInfoAbout = document.querySelector(".profile__info-explorer");
+const profileInfoName = document.querySelector("#profName");
+const profileInfoAbout = document.querySelector("#profAbout");
 
-const popupFormName = document.querySelector(
-  ".popup__container-form-inputs_name"
-);
-const popupFormAbout = document.querySelector(
-  ".popup__container-form-inputs_about"
-);
-const buttonSubmit = document.querySelector(
-  ".popup__container-form-inputs_button"
-);
+const popupFormName = document.querySelector("#popupName");
+const popupFormAbout = document.querySelector("#popupAbout");
+const buttonSubmit = document.querySelector("#btnSubmit");
+//Variables para agregar nueva imagen
+const addBtn = document.querySelector("#addImg");
+const popUpAdd = document.querySelector("#popUpAdd");
+const closePopUpAdd = document.querySelector("#closeAddBtn");
 
+//Variables para crear las tarjetas iniciales con JS
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -44,6 +41,30 @@ const initialCards = [
   },
 ];
 
+const cardContainer = document.querySelector(".elements");
+
+const templateCard = initialCards.forEach((element) => {
+  const card = document.createElement("div");
+  card.classList.add("elements__element");
+  cardContainer.append(card);
+  const likeButton = document.createElement("img");
+  likeButton.classList.add("elements__element-button");
+  likeButton.src = "./images/like_button.svg";
+  card.append(likeButton);
+  const image = document.createElement("img");
+  image.classList.add("elements__element-image");
+  image.src = element.link;
+  card.append(image);
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("elements__element-text");
+  paragraph.textContent = element.name;
+  card.append(paragraph);
+  const rectangle = document.createElement("img");
+  rectangle.classList.add("elements__element-rectangle");
+  rectangle.src = "./images/Rectangle.png";
+  card.append(rectangle);
+});
+
 function openPopUp() {
   popUp.classList.add("popup__opened");
   popupFormName.value = profileInfoName.textContent;
@@ -60,7 +81,15 @@ function handleProfileFormSubmit(evt) {
   profileInfoAbout.textContent = popupFormAbout.value;
   closePopUp();
 }
+function openAddPopUp() {
+  popUpAdd.classList.add("popup__opened");
+}
+function closeAddPopUp() {
+  popUpAdd.classList.remove("popup__opened");
+}
 
 editBtn.addEventListener("click", openPopUp);
 closePopUpBtn.addEventListener("click", closePopUp);
 buttonSubmit.addEventListener("click", handleProfileFormSubmit);
+addBtn.addEventListener("click", openAddPopUp);
+closePopUpAdd.addEventListener("click", closeAddPopUp);
